21209a5c3e6ebc6fb0a265e086b7b7370d4703fc
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2023-06-30 14:41:11 +0700

    close #27 - Delete all objects

--- node-js-crud-sql-server/app/controllers/tutorial.controller.js
+++ node-js-crud-sql-server/app/controllers/tutorial.controller.js
@@ -112,7 +112,18 @@ exports.delete = (req, res) => {
 
 // Delete all Tutorials from the database.
 exports.deleteAll = (req, res) => {
-  
+    Tutorial.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} Tutorials were deleted successfully!`
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all tutorials."
+      });
+    });
 };
 
 // Find all published Tutorials
