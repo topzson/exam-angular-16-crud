8072729c92253918f55032f10d71801c735bed86
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2023-06-30 13:58:21 +0700

    close #19 close #20 - Define Mongoose and model

---
+++ node-js-crud-sql-server/app/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports={
+    url:"mongodb+srv://Topzson:rattatammanoon1@cluster0.8zqysz0.mongodb.net/"
+};
--- node-js-crud-sql-server/server.js
+++ node-js-crud-sql-server/server.js
@@ -22,3 +22,17 @@ app.listen(PORT, () => {
 
     console.log(`Server is running on port ${PORT}.`);
 });
+
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
