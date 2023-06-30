c93b5ae98ca1f5a8d76caf2391d8c74276ac1451
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2023-06-30 14:24:54 +0700

    close #23 - Retrieve objects (with condition)

--- node-js-crud-sql-server/app/controllers/tutorial.controller.js
+++ node-js-crud-sql-server/app/controllers/tutorial.controller.js
@@ -31,7 +31,18 @@ exports.create = (req, res) => {
 
 // Retrieve all Tutorials from the database.
 exports.findAll = (req, res) => {
-  
+  const title = req.query.title;
+  var condition = title ? { title: {$reqex: new RegExp(title), $options: "i"}} : {};
+
+  Tutorial.find(condition).then(data=>{
+    res.send(data);
+  })
+  .catch(err=>{
+    res.status(500).send({
+        message:
+            err.message || "Some error occurred while retrieving tutorials."
+    });
+  });
 };
 
 // Find a single Tutorial with an id
